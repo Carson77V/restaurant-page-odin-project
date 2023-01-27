78f44971114be4c50bab04cdc8e8731b058c6d77
Carson <carsontwj@gmail.com>
2023-01-26 22:35:33 -0500

    Add eventListener to each button and del content
    When a button is clicked, check if what content is on webpage. Delete
    content if the user wants to load new content.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ import homepage from "./pages/homepage";
 import aboutPage from "./pages/about";
 import menuPage from "./pages/menu";
 
-//homepage()
-//aboutPage()
-menuPage()
+//auto load homepage
+homepage()
+
+const btns = document.querySelectorAll('button')
+
+//add eventListener to all buttons
+btns.forEach(btn => {
+    btn.addEventListener('click', event => {
+        const currentContent = document.querySelector('#content > div')
+        //check what content is already loaded by comparing btn id with 
+        //content class. If they are different then load the new content
+        if (event.target.id != currentContent.classList.value) {
+            //remove current content
+            currentContent.remove()
+        }
+    })
+})
+
